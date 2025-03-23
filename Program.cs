c0b1429162ee8d6e8932a44833adce04ad9eeb53
Игорь <@DESKTOP-K8QM5EE>
2025-03-23 21:35:52 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите свой возраст:");
+        int age = Convert.ToInt32(Console.ReadLine());
+        if (age >= 18) { Console.WriteLine("Вы совершеннолетний"); }
+        else if (age > 0 && age < 18) { Console.WriteLine("Вы ещё несовершеннолетний"); }
+        else { Console.WriteLine("Введите коррекктное значенние"); }
+    }
+}
